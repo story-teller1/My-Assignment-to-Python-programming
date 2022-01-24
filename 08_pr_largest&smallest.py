701bde1a46def5be16c502a4424bf29a9939a21a
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 19:08:44 +0530

    Update 08_pr_largest&smallest.py

--- 08_pr_largest&smallest.py
+++ 08_pr_largest&smallest.py
@@ -1,24 +1,29 @@
+try:
 #inputs three numbers from the user
-n1=eval(input("\nEnter the 1st number : "))
-n2=eval(input("Enter the 2nd number : "))
-n3=eval(input("Enter the 3rd number : "))
+    n1=eval(input("\nEnter the 1st number : "))
+    n2=eval(input("Enter the 2nd number : "))
+    n3=eval(input("Enter the 3rd number : "))
 
 #checking which number is greater among the first two numbers
-if n1>n2:
-    n=n1
-    s=n2
-else:
-    n=n2
-    s=n1
+    if n1>n2:
+        n=n1
+        s=n2
+    else:
+        n=n2
+        s=n1
     
 #comparing the first two numbers with the third one
-if n>n3:
-    print(f"\nThe greatest number among the numbers {n1}, {n2} and {n3} is {n}")
-    if s>n3:
-        print(f"The smallest number among the numbers {n1}, {n2} and {n3} is {n3}\n")
-    else:
-        print(f"The smallest number among the numbers {n1}, {n2} and {n3} is {s}\n")
+    if n>n3:
+        print(f"\nThe greatest number among the numbers {n1}, {n2} and {n3} is {n}")
+        if s>n3:
+            print(f"The smallest number among the numbers {n1}, {n2} and {n3} is {n3}\n")
+        else:
+            print(f"The smallest number among the numbers {n1}, {n2} and {n3} is {s}\n")
     
-else:
-    print(f"\nThe greatest number among the numbers {n1}, {n2} and {n3} is {n3}")
-    print(f"The smallest number among the numbers {n1}, {n2} and {n3} is {s}\n")
+    else:
+        print(f"\nThe greatest number among the numbers {n1}, {n2} and {n3} is {n3}")
+        print(f"The smallest number among the numbers {n1}, {n2} and {n3} is {s}\n")
+
+except:
+    #handling the program from the invalid inputs by the user
+    print("\nInvalid input!\nPlease enter a numeric value.\n")
