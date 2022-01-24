3581a1b0128bb956207ce8d7a3205b4a5dab5767
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 18:10:05 +0530

    Update 05_pr_temperature.py

--- 05_pr_temperature.py
+++ 05_pr_temperature.py
@@ -1,3 +1,8 @@
+#inputs a temperature in degree Celsius from the user
 C=eval(input("\nEnter the temperature in degree Celsius : "))
+
+#converting the temperature in degree Fahrenheit from Celsius
 F=9*C/5+32
+
+#printing the results
 print(f"\n{C}°C is equal to {F}°F \nWhere C=Celsius & F=Fahrenheit.\n")
