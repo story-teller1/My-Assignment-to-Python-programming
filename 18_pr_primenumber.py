4f3a8fb438318ab530ede2382bccd08532d2660d
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 19:41:38 +0530

    Update 18_pr_primenumber.py

--- 18_pr_primenumber.py
+++ 18_pr_primenumber.py
@@ -1,14 +1,23 @@
 try:
+    
+    #inputs a number from the user
     n=eval(input("\nEnter any number : "))
-
+    
+    #defining a variable to check whether the number is prime or not
     prime= True
-    while n<0:
+    
+    #making sure that the given number is positive
+    while n<=0:
         n=eval(input("\nInvalid input!\nEnter any natural number : "))
+        
+    #conditions for being prime
     if n>0:
         for i in range(2,n):
             if n%i==0:
                prime= False
                break
+      
+    #checking whether the number fulfils the condition
         if prime:
             print(f"\n{n} is a prime number.\n")
 
@@ -18,6 +27,7 @@ try:
             
     
 except:
+    #handling the program from the invalid inputs by the user
     print("\nPlease enter a numerical value.\n")
 
     
