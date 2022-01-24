04d493a9abeac72a3bc7ac71ebd5dd2479982da8
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 18:54:01 +0530

    Update 02_pr_area & perimeter of a rectangle.py

--- 02_pr_area & perimeter of a rectangle.py
+++ 02_pr_area & perimeter of a rectangle.py
@@ -1,15 +1,20 @@
+try:
 #inputs the length & breadth of the rectangle from the user
-l=eval(input("Enter the length of the rectangle : "))
-b=eval(input("Enter the breadth of the rectangle : "))
+  l=eval(input("Enter the length of the rectangle : "))
+  b=eval(input("Enter the breadth of the rectangle : "))
 
 
 #finding the area of the rectangle
-a=l*b
+  a=l*b
 
 #finding the perimetre of the rectangle
-p=(l+b)*2
+  p=(l+b)*2
 
 #printing the results
-print(f"The area of the rectangle is {a}")
-print(f"The perimeter of the rectangle is {p}")
+  print(f"The area of the rectangle is {a}")
+  print(f"The perimeter of the rectangle is {p}")
 
+except:
+  #handling the program from invalid inputs
+  print("Invalid input!\nPlease enter a numeric value.")
+ 
