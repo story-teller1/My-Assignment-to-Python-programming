fd1857f20a37facc174397f6dd69768f2fd92d30
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 18:21:08 +0530

    Update 08_pr_largest&smallest.py

--- 08_pr_largest&smallest.py
+++ 08_pr_largest&smallest.py
@@ -1,12 +1,17 @@
+#inputs three numbers from the user
 n1=eval(input("\nEnter the 1st number : "))
 n2=eval(input("Enter the 2nd number : "))
 n3=eval(input("Enter the 3rd number : "))
+
+#checking which number is greater among the first two numbers
 if n1>n2:
     n=n1
     s=n2
 else:
     n=n2
     s=n1
+    
+#comparing the first two numbers with the third one
 if n>n3:
     print(f"\nThe greatest number among the numbers {n1}, {n2} and {n3} is {n}")
     if s>n3:
