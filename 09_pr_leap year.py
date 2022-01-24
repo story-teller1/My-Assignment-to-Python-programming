f6cee8a0c8f8e9229ffe1a7178aaeefefc1c056a
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 19:10:12 +0530

    Update 09_pr_leap year.py

--- 09_pr_leap year.py
+++ 09_pr_leap year.py
@@ -1,16 +1,21 @@
+try:
 #inputs a year from the user
-y=int(input("\nEnter a year : "))
+    y=int(input("\nEnter a year : "))
 
 #checks the century years whether it is a leap year or not
-if y%100==0:
-    if y%400==0:
-        print(f"\nThe year {y} is a leap year.\n")
-    else:
-        print(f"\nThe year {y} is not a leap year.\n")
+    if y%100==0:
+        if y%400==0:
+            print(f"\nThe year {y} is a leap year.\n")
+        else:
+            print(f"\nThe year {y} is not a leap year.\n")
         
 #checks the years other than the century years whether it's a leap year or not
-else:
-    if y%4==0:
-        print(f"\nThe year {y} is a leap year.\n")
     else:
-        print(f"\nThe year {y} is not a leap year.\n")
+        if y%4==0:
+            print(f"\nThe year {y} is a leap year.\n")
+        else:
+            print(f"\nThe year {y} is not a leap year.\n")
+
+except:
+    #handling the program from the invalid inputs by the user
+    print("\nInvalid input!\nPlease enter a numeric value.\n")
