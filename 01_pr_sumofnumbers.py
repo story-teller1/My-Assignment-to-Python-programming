54ae892f68fedff79a2938adffb3d1df886f4828
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 18:50:15 +0530

    Update 01_pr_sumofnumbers.py

--- 01_pr_sumofnumbers.py
+++ 01_pr_sumofnumbers.py
@@ -1,9 +1,13 @@
+try:
 #inputs two numbers a & b from the user
-a =int(input("Enter the first value : "))
-b= int(input("Enter the second value : "))
+  a =int(input("Enter the first value : "))
+  b= int(input("Enter the second value : "))
 
 #adds the two numbers
-c = a + b	
+  c = a + b	
 
 #printing the result
-print (f"The sum of {a} and {b} is {c}.")
+  print (f"The sum of {a} and {b} is {c}.")
+except:
+  #handling the program from invalid inputs from the user
+  print ("Invalid input!\nPlease enter a numeric value.")
