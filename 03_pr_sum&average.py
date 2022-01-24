b30831a5b11f7369c67873620c1936f771838a89
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 18:56:30 +0530

    Update 03_pr_sum&average.py

--- 03_pr_sum&average.py
+++ 03_pr_sum&average.py
@@ -1,14 +1,20 @@
+try:
+  
 #inputs three numbers from the user
-n1=eval(input("Enter the first decimal number : "))
-n2=eval(input("Enter the second decimal number : "))
-n3=eval(input("Enter the third decimal number : "))
+  n1=eval(input("Enter the first decimal number : "))
+  n2=eval(input("Enter the second decimal number : "))
+  n3=eval(input("Enter the third decimal number : "))
 
 #finding the sum of the numbers
-sum=n1+n2+n3
+  sum=n1+n2+n3
 
 #finding the average of the numbers
-average=(n1+n2+n3)/3
+  average=(n1+n2+n3)/3
 
 #printing the results
-print(f"The sum of the numbers {n1}, {n2} and {n3} is : {sum}")
-print(f"The average of the numbers {n1}, {n2} and {n3} is : {average}")
+  print(f"The sum of the numbers {n1}, {n2} and {n3} is : {sum}")
+  print(f"The average of the numbers {n1}, {n2} and {n3} is : {average}")
+
+except:
+   #handling the program from invalid inputs by the user
+    print("Invalid input!\nPlease enter a numeric value.")
