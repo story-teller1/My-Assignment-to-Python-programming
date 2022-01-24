83c238d0a7833dabff86b5481e0a0ac1ef5c1048
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 19:04:19 +0530

    Update 06_pr_absolute value.py

--- 06_pr_absolute value.py
+++ 06_pr_absolute value.py
@@ -1,9 +1,14 @@
+try:
 #inputs a number from the user
-n=eval(input("\nEnter any number : "))
+    n=eval(input("\nEnter any number : "))
 
 #finding the absolute value of the number
-if n>=0:
-    print(f"\nThe absolute value of the given number {n} is : {n}\n")
+    if n>=0:
+        print(f"\nThe absolute value of the given number {n} is : {n}\n")
 
-else:
-    print(f"\nThe absolute value of the given number {n} is : {-n}\n")
+    else:
+        print(f"\nThe absolute value of the given number {n} is : {-n}\n")
+
+except:
+    #handling the program from the invalid inputs by the user
+    print("\nInvalid input!\nPlease enter a numeric value.\n")
