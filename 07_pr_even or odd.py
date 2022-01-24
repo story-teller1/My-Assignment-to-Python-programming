048d9474dd93057840b14f1fc6e3f0fa7578b398
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 19:06:11 +0530

    Update 07_pr_even or odd.py

--- 07_pr_even or odd.py
+++ 07_pr_even or odd.py
@@ -1,10 +1,15 @@
+try:
 #inputs a number from the user
-n=eval(input("\nEnter any number : "))
+    n=eval(input("\nEnter any number : "))
 
 #checking the number whether it is even
-if n%2==0:
-    print(f"\nThe given number {n} is even.\n")
+    if n%2==0:
+        print(f"\nThe given number {n} is even.\n")
 
 #checking the number whether it is odd    
-else:
-    print(f"\nThe given number {n} is odd.\n")
+    else:
+        print(f"\nThe given number {n} is odd.\n")
+
+except:
+    #handling the program from the invalid inputs by the user
+    print("\nInvalid input!\nPlease enter a numeric value.\n")
