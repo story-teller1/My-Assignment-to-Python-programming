da793977d55f57711100095cbd4e3c737596f318
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 19:31:59 +0530

    Update 16_pr_sumofdigits.py

--- 16_pr_sumofdigits.py
+++ 16_pr_sumofdigits.py
@@ -1,9 +1,21 @@
-n=int(input("\nEnter a number : "))
-n1=n
-sum=0
-while n>0:
-    r=n%10
-    sum+=r
-    n=n//10
-
-print(f"\nThe sum of the digits of the number {n1} is : {sum}\n")
+try:
+    #inputs a number from the user
+    n=int(input("\nEnter a number : "))
+    
+    #defining  variables to store the given number and the sum of the digits respectively
+    n1=n
+    sum=0
+    
+    #calculating the sum of the digits
+    while n>0:
+        r=n%10
+        sum+=r
+        n=n//10
+        
+        
+    #printing the results
+    print(f"\nThe sum of the digits of the number {n1} is : {sum}\n")
+    
+except:
+    #handling the program from the invalid inputs by the user
+    print("\nInvalid input!\nPlease enter a numeric value.\n")
