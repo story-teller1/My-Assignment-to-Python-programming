3ebb36a4d118b62a96e3e3218b8cb0009be1a9ca
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 18:34:45 +0530

    Update 12_pr_(c)pattern.py

--- 12_pr_(c)pattern.py
+++ 12_pr_(c)pattern.py
@@ -1,5 +1,8 @@
+#inputs a number from the user up to which number of line he or she wants to print the pattern
 n=int(input("\nEnter the number up to which number of line you want to print the pattern : "))
 print()
+
+#printing the pattern
 for i in range(1,n+1):
     print(" "*(n-i)+"*"*(2*i-1)+" ")
     
