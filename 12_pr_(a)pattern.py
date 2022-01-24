beeb76159754fd6b11accef336b1d557712f16cf
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 18:33:09 +0530

    Update 12_pr_(a)pattern.py

--- 12_pr_(a)pattern.py
+++ 12_pr_(a)pattern.py
@@ -1,5 +1,8 @@
+#inputs a number from the user up to which line the user wants to print the pattern
 n=int(input("\nEnter the number up to which number of line you want to print the pattern : "))
 print()
+
+#prints the required pattern
 for i in range(1,n+1):
     print("*"*i)
 print()
