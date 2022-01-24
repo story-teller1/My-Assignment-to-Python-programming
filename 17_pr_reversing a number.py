d1abffc780880bea4017ef0e71e2f6a664b29fe8
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 19:35:30 +0530

    Update 17_pr_reversing a number.py

--- 17_pr_reversing a number.py
+++ 17_pr_reversing a number.py
@@ -1,9 +1,20 @@
-n=int(input("\nEnter a number : "))
-n1=n
-reverse_n=0
-while n>0:
-    r=n%10
-    reverse_n=reverse_n*10 + r
-    n=n//10
+try:
+    #inputs a number from the user
+    n=int(input("\nEnter a number : "))
+    
+    #defining a variable to store the value of the given number and the reversed number
+    n1=n
+    reverse_n=0
+    
+    #calculation of the reversed number
+    while n>0:
+        r=n%10
+        reverse_n=reverse_n*10 + r
+        n=n//10
 
-print(f"\nThe reverse of the digits of the number {n1} is : {reverse_n}\n")
+    #printing the results
+    print(f"\nThe reverse of the digits of the number {n1} is : {reverse_n}\n")
+    
+except:
+    #handling the program from the invalid inputs by the user
+    print("\nInvalid input!\nPlease enter a numeric value.\n")
