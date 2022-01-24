dc587dfcb0625854207707ddd6bd92a4cdaca20c
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 18:01:37 +0530

    Update 01_pr_sumofnumbers.py

--- 01_pr_sumofnumbers.py
+++ 01_pr_sumofnumbers.py
@@ -1,4 +1,9 @@
+#inputs two numbers a & b from the user
 a =int(input("Enter the first value : "))
 b= int(input("Enter the second value : "))
+
+#adds the two numbers
 c = a + b	
+
+#printing the result
 print (f"The sum of {a} and {b} is {c}.")
