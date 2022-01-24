4bd49c6612c5a4538bdf67afad584efe71876807
Jit Mondal <97719835+JitaDeveloper@users.noreply.github.com>
2022-01-24 19:12:31 +0530

    Update 11_pr_solving quadratic equation.py

--- 11_pr_solving quadratic equation.py
+++ 11_pr_solving quadratic equation.py
@@ -1,45 +1,49 @@
 # solving a quadratic equation
 #instructing the user what to do, like how to enter all the required information, in which format etc
 print("\nWe will solve a quadratic equation having this form : \n\t\tax^2 + bx + c = 0\nPlease Enter the required information below :")
-a=eval(input("\nEnter the coefficient of x^2 (a) : "))
-b=eval(input("Enter the coefficient of x   (b) : "))
-c=eval(input("Enter the constant term      (c) : "))
+try:
+    a=eval(input("\nEnter the coefficient of x^2 (a) : "))
+    b=eval(input("Enter the coefficient of x   (b) : "))
+    c=eval(input("Enter the constant term      (c) : "))
 
 #adjusting the '+' and the '-' sign of the coefficients & constant terms
-if b>=0:
-    p="+ "+str(b)
-    if b==1:
-        p="+ "
+    if b>=0:
+        p="+ "+str(b)
+        if b==1:
+            p="+ "
     
-else:
-    p="- "+str(-b)
-    if b==-1:
-        p="- "
-if a==1:
-        q=" "
-elif a==-1:
-    q="-"
-if c>=0:
-    r="+ "+str(c)
-    if c==1:
-        r="+ 1"
+    else:
+        p="- "+str(-b)
+        if b==-1:
+            p="- "
+    if a==1:
+            q=" "
+    elif a==-1:
+        q="-"
+    if c>=0:
+        r="+ "+str(c)
+        if c==1:
+            r="+ 1"
     
-else:
-    r="- "+str(-c)
-    if c==-1:
-        r="- 1"
+    else:
+        r="- "+str(-c)
+        if c==-1:
+            r="- 1"
         
 #defining the discriminant of the quadratic equation
-d=(b**2-4*a*c)
+    d=(b**2-4*a*c)
 
 #checking whether the equation has real roots or not
-if d<0:
-    print("\nThis equation has no real roots.")
+    if d<0:
+        print("\nThis equation has no real roots.")
     
     
 #if the equation has real roots then finds it and print them
-else:
-    x1=(-b+(d)**0.5)/(2*a)
-    x2=(-b-(d)**0.5)/(2*a)
-    print(f"\nThe required solutions of the equation \n\t  {q}x^2 {p}x {r} = 0\n are {x1} and {x2} respectively.")
+    else:
+        x1=(-b+(d)**0.5)/(2*a)
+        x2=(-b-(d)**0.5)/(2*a)
+        print(f"\nThe required solutions of the equation \n\t  {q}x^2 {p}x {r} = 0\n are {x1} and {x2} respectively.")
     
+except:
+    #handling the program from the invalid inputs by the user
+    print("\nInvalid input!\nPlease enter a numeric value.\n")
